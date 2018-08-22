b00faf347c1413b6d4814cbccc9f5afb9d17c188
Pradeep Soundalgekar <41544614+pradeepp37@users.noreply.github.com>
2018-08-21 23:09:43 -1100

    Create array_main.java

---
+++ array_main.java
@@ -0,0 +1,5 @@
+//Program : Write a function that prints all the elements of an array of integers, one per line.
+//          The parameters to the function sholud be int arr[] and int size.
+//          NOTE : No "while / for" loops allowed.
+//
+// Code: 
